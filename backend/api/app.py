f891df1ec6c765bab6ff904b8635fae4530d499b
Sanjay V <sanjay.venkat@sifycorp.com>
2025-08-05 19:13:44 +0530

    docker image

--- backend/api/app.py
+++ backend/api/app.py
@@ -9,13 +9,3 @@ CORS(app)
 app.register_blueprint(errors)
 app.register_blueprint(clusters_bp)
 swagger = Swagger(app, template_file="../docs/swagger.yaml")
-
-
-@app.route("/")
-def index():
-    return Response("Hello, world!", status=200)
-
-
-@app.route("/health")
-def health():
-    return Response("OK", status=200)
