d191e5f53c952217c39642064b45b264920a036b
harishsify13 <168715357+harishsify13@users.noreply.github.com>
2025-08-05 15:47:29 +0530

    apis

--- backend/api/clusters/routes.py
+++ backend/api/clusters/routes.py
@@ -1,15 +1,21 @@
 # clusters/routes.py
 
 from flask import Blueprint, jsonify
-from .controllers import get_cluster_status, get_node_info
+from .controllers import get_cluster_status, get_node_info , summaryApi
 
 
-clusters_bp = Blueprint('clusters', __name__, url_prefix='/clusters')
+clusters_bp = Blueprint('clusters', __name__, url_prefix='/api/v1')
 
-@clusters_bp.route('/status', methods=['GET'])
+@clusters_bp.route('/clusters/status', methods=['GET'])
 def cluster_status():
     return jsonify(get_cluster_status())
 
-@clusters_bp.route('/nodes', methods=['GET'])
+
+
+@clusters_bp.route('/all', methods=['GET'])
+def summary():
+    return jsonify(summaryApi())
+
+@clusters_bp.route('/clusters/nodes', methods=['GET'])
 def node_info():
     return jsonify(get_node_info())
