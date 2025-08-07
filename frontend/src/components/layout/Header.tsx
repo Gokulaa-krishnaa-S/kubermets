2caf3c0366a60d788a30f7d752c1978e2e2dfc34
Selvakannan M <selva.kannan@sifycorp.com>
2025-08-07 14:46:25 +0530

    selva

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -48,7 +48,7 @@ export function Header({
           </div> */}
 
           {/* Theme Toggle */}
-          <Button
+          {/* <Button
             variant="ghost"
             size="icon"
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
@@ -57,7 +57,7 @@ export function Header({
             <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             <span className="sr-only">Toggle theme</span>
-          </Button>
+          </Button> */}
 
           {/* Notifications */}
           {/* <Button variant="ghost" size="icon" className="relative">
